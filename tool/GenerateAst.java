bd59a78ae5b4e81247cfbcb40834436ab91c2a35
FLinLan <maxlan0987@gmail.com>
2024-12-15 10:49:25 +0800

    implmented the visitor pattern within the abstract syntax tree gneerator

--- tool/GenerateAst.java
+++ tool/GenerateAst.java
@@ -37,6 +37,10 @@ public class GenerateAst {
             defineType(writer, baseName, className, fields);
         }
 
+        // The base accept() method.
+        writer.println();
+        writer.println("  abstract <R> R accept(Visitor<R> visitor);");
+
         writer.println("}");
         writer.close();
     } 
@@ -56,12 +60,30 @@ public class GenerateAst {
 
         writer.println("    }");
 
+        // Visitor pattern.
+        writer.println();
+        writer.println("    @Override");
+        writer.println("    <R> R accept(Visitor<R> visitor) {");
+        writer.println("      return visitor.visit" + className + baseName + "(this);");
+        writer.println("     }");
+
         // Fields
         writer.println();
         for (Stirng field : fields) {
             writer.println("  final " + field + ";");
         }
-        
+
         writer.pritnln("   }");
+    }
+
+    private static void defineVisitor(PriterWriter writer, String baseName, List<String> types) {
+        writer.println("   interface Visitor<R> {");
+
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("    R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+        }
+
+        writer.println("  }");
     }
 }
