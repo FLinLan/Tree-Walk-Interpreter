1d82ce2ef28636c62e5b92602fe8a64658755aa1
FLinLan <maxlan0987@gmail.com>
2024-12-27 01:56:40 +0800

    added binary expr function and boolean true value

--- java/com/craftinginterpreters/lox/Interpreter.java
+++ java/com/craftinginterpreters/lox/Interpreter.java
@@ -16,6 +16,57 @@ class Interpreter implements Expr.Visitor<Object> {
         return expr.accept(this);
     }
 
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr) {
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case PLUS:
+                if (left instanceof Double && right instanceof Double) {
+                    return (double) left + (double) right;
+                }
+                throw new RuntimeException("Operands must be numbers.");
+            case MINUS:
+                return (double) left - (double) right;
+            case STAR:
+                return (double) left * (double) right;
+            case SLASH:
+                if ((double) right!= 0) {
+                    return (double) left / (double) right;
+                }
+                throw new RuntimeException("Division by zero.");
+            case BANG_EQUAL:
+                return isEqual(left, right);
+            case EQUAL_EQUAL:
+                return isEqual(left, right);
+        }
+
+        return null; // TOD
+    }
+
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+        // Implementation of unary expression evaluation
+        // and return the result
+        Object right = evaluate(expr.expression);
+
+        switch (expr.operator.type) {
+            case BANG:
+                return !isTruthy(right);
+            case MINUS:
+            return -(double)right;
+        }
+
+        return null; // TOD
+    }
+
+    private boolean isTruthy(Object value) {
+        if (value == null) return false;
+        if (value instanceof Boolean) return (boolean) value;
+        return true;
+    }
+
 
 
 }
