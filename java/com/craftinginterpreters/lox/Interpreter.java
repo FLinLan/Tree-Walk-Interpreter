817cfa7bbbc25512eb4727b83a49f0f5a4ccc311
FLinLan <maxlan0987@gmail.com>
2024-12-27 13:12:28 +0800

    added operand checking for binary operators

--- java/com/craftinginterpreters/lox/Interpreter.java
+++ java/com/craftinginterpreters/lox/Interpreter.java
@@ -23,23 +23,34 @@ class Interpreter implements Expr.Visitor<Object> {
 
         switch (expr.operator.type) {
             case GREATER:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left > (double)right;
             case GREATER_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left >= (double)right;
             case LESS:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left < (double)right;
             case LESS_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left <= (double)right;
             case PLUS:
+                checkNumberOperands(expr.operator, left, right);
                 if (left instanceof Double && right instanceof Double) {
                     return (double) left + (double) right;
                 }
-                throw new RuntimeException("Operands must be numbers.");
+                if (left instanceof String && right instanceof String) {
+                    return (String) left + (String) right;
+                }
+                throw new RuntimeException("Operands must two numbers of two strings.");
             case MINUS:
+                checkNumberOperands(expr.operator, left, right);
                 return (double) left - (double) right;
             case STAR:
+                checkNumberOperands(expr.operator, left, right);
                 return (double) left * (double) right;
             case SLASH:
+                checkNumberOperands(expr.operator, left, right);
                 if ((double) right!= 0) {
                     return (double) left / (double) right;
                 }
@@ -70,10 +81,17 @@ class Interpreter implements Expr.Visitor<Object> {
         return null; // TOD
     }
 
+    // For unary operators
     private void checkNumberOperand(Token operator, Object operand) {
         if (!(operand instanceof Double)) {
             throw new RuntimeError(operator, "Operand must be a number.");
         }
+    }
+
+    // For binary operators
+    private void checkNumberOperands(Token operator, Object left, Object right) {
+        if (left instanceof Double && right instanceof Double) return;
+        throw new RuntimeError(operator, "Operands must be numbers.");
     }
 
     private boolean isTruthy(Object value) {
@@ -87,7 +105,4 @@ class Interpreter implements Expr.Visitor<Object> {
         if (a == null || b == null) return false;
         return a.equals(b);
     }
-
-
-
 }
